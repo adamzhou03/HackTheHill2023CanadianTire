ae61d2ef9a28c30870286122495ecca8b6694be6
adamzhou03 <90469720+adamzhou03@users.noreply.github.com>
2023-03-03 23:29:35 -0500

    webpart base

---
+++ src/webparts/hackTheHill2023CanadianTireMain/components/IHackTheHill2023CanadianTireMainProps.ts
@@ -0,0 +1,7 @@
+export interface IHackTheHill2023CanadianTireMainProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/hackTheHill2023CanadianTireMain/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IHackTheHill2023CanadianTireMainWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'HackTheHill2023CanadianTireMainWebPartStrings' {
+  const strings: IHackTheHill2023CanadianTireMainWebPartStrings;
+  export = strings;
+}
